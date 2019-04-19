cc5d51b1a13d8c3b84acb6d89b9281e9220831b6
Fadli Ramadhan <fadlir93@gmail.com>
2019-04-19 19:02:53 +0700

    Create Approval Card

--- src/index.js
+++ src/index.js
@@ -2,28 +2,38 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import faker from 'faker';
 import CommentDetail from './CommentDetail'
+import ApprovalCard from './ApprovalCard'
 
 const App = () => {
     return (
         <div className="ui container comments"> 
-            <CommentDetail 
-                author= "Fadli" 
-                timeAgo="Today at 3:40PM" 
-                comment="This no solution"
-                avatar= {faker.image.avatar()}
-            />
-            <CommentDetail 
-                author= "Alex" 
-                timeAgo="Today at 10.00PM" 
-                comment="Actually this good for me"
-                avatar= {faker.image.avatar()}
-            />
-            <CommentDetail 
-                author= "Sam" 
-                timeAgo="Today at 11.00AM" 
-                comment="Nice blog post!"
-                avatar= {faker.image.avatar()}
-            />
+            <ApprovalCard > 
+                <CommentDetail 
+                    author= "Fadli" 
+                    timeAgo="Today at 3:40PM" 
+                    comment="This no solution"
+                    avatar= {faker.image.avatar()}
+                />
+            </ApprovalCard>
+
+            <ApprovalCard>
+                <CommentDetail 
+                    author= "Alex" 
+                    timeAgo="Today at 10.00PM" 
+                    comment="Actually this good for me"
+                    avatar= {faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
+            <ApprovalCard>
+                <CommentDetail 
+                    author= "Sam" 
+                    timeAgo="Today at 11.00AM" 
+                    comment="Nice blog post!"
+                    avatar= {faker.image.avatar()}
+                />
+            </ApprovalCard>
+            
         </div>
     )
 }
