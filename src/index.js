8391733aa2555ea4ff3d2a87f5b5c67f7e40cf02
Fadli Ramadhan <fadlir93@gmail.com>
2019-04-19 17:54:59 +0700

    Multiple props

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import CommentDetail from './CommentDetail'
 const App = () => {
     return (
         <div className="ui container comments"> 
-            <CommentDetail author= "Fadli"/>
-            <CommentDetail author= "Alex"/>
-            <CommentDetail author= "Sam"/>
+            <CommentDetail 
+                author= "Fadli" 
+                timeAgo="Today at 3:40PM" 
+                comment="This no solution"
+                avatar= {faker.image.avatar()}
+            />
+            <CommentDetail 
+                author= "Alex" 
+                timeAgo="Today at 10.00PM" 
+                comment="Actually this good for me"
+                avatar= {faker.image.avatar()}
+            />
+            <CommentDetail 
+                author= "Sam" 
+                timeAgo="Today at 11.00AM" 
+                comment="Nice blog post!"
+                avatar= {faker.image.avatar()}
+            />
         </div>
     )
 }
